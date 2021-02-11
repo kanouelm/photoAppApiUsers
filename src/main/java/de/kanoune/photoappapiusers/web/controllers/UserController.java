816fa1a5783928ae27dee868378e566c1062c445
kanouelm <ekanoune@yahoo.com>
2021-02-11 11:46:36 +0100

    "Zuul as a Load Balancer: here we would start more than once instance of the service photoAppApiUser through the customizing of properties file with server.port=${PORT:0} and adding the property: eureka.instance.instance-id=${spring.application.name}:${spring.application.instance_id:${random.value}}

--- src/main/java/de/kanoune/photoappapiusers/web/controllers/UserController.java
+++ src/main/java/de/kanoune/photoappapiusers/web/controllers/UserController.java
@@ -1,5 +1,7 @@
 package de.kanoune.photoappapiusers.web.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/users")
 public class UserController {
 
+    @Autowired
+    private Environment environment;
     @GetMapping("/status/check")
     public String getStatus() {
-        return "PhotoAppApiUsers service is working!";
+
+        return "PhotoAppApiUsers service is working and is running on the port " + environment.getProperty("local.server.port");
     }
 }
