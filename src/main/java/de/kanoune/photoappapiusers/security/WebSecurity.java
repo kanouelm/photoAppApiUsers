ba95eb8a3f54cb3164bb0350b0d93cacf8db8b36
kanouelm <ekanoune@yahoo.com>
2021-02-14 19:20:54 +0100

    Section 12: User Microservice:Allow only IP address of Zuul API Gateway and Adding Support to Return JSON or XML

--- src/main/java/de/kanoune/photoappapiusers/security/WebSecurity.java
+++ src/main/java/de/kanoune/photoappapiusers/security/WebSecurity.java
@@ -1,6 +1,7 @@
 package de.kanoune.photoappapiusers.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,11 +10,18 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
+    private final Environment environment;
+
+    public WebSecurity(Environment environment) {
+        this.environment = environment;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //super.configure(http);
         http.csrf().disable();
         http.authorizeRequests().antMatchers("/api/v1/users/**").permitAll();
+        //http.authorizeRequests().antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"));
         http.headers().frameOptions().disable();
     }
 }
--- src/main/java/de/kanoune/photoappapiusers/web/controllers/UserController.java
+++ src/main/java/de/kanoune/photoappapiusers/web/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -32,7 +33,10 @@ public class UserController {
         return "PhotoAppApiUsers service is working and is running on the port " + environment.getProperty("local.server.port");
     }
 
-    @PostMapping
+    @PostMapping(
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+    )
     public ResponseEntity<UserResponse> createUser(@Valid @RequestBody  UserRequest requestedUserDetails) {
 
         ModelMapper modelMapper = new ModelMapper();
