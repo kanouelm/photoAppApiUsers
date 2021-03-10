2a49f6fddb874dccf5c19418356bc0f92da4f0ca
kanouelm <ekanoune@yahoo.com>
2021-03-10 22:43:25 +0100

    Section 24 - Microservices Communication(Spring Rest Template): Make Users Microservice call Albums Microservice

--- src/main/java/de/kanoune/photoappapiusers/PhotoAppApiUsersApplication.java
+++ src/main/java/de/kanoune/photoappapiusers/PhotoAppApiUsersApplication.java
@@ -5,16 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 
 @SpringBootApplication
@@ -34,14 +30,8 @@ public class PhotoAppApiUsersApplication {
 	@Bean
 	@LoadBalanced
 	public RestTemplate getRestTemplate() {
-		final RestTemplate restTemplate = new RestTemplate();
-		List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
-		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-		converter.setSupportedMediaTypes(Collections.singletonList(MediaType.ALL));
-		messageConverters.add(converter);
-		restTemplate.setMessageConverters(messageConverters);
 
-		return restTemplate;
+		return new RestTemplate();
 	}
 
 }
