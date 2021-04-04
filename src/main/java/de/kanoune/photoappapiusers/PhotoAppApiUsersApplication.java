99ea6eb7a364d439654ec9bc57ead2fa25296c01
kanouelm <ekanoune@yahoo.com>
2021-04-04 22:15:55 +0200

    Section 24 - Microservices Communication: Hystrix Circuit Breaker and Feign Client and Error Handling with Feign Hystrix FallbackFactory: This is not working because @EnableCircuitBreaker annotation is deprecated

--- src/main/java/de/kanoune/photoappapiusers/PhotoAppApiUsersApplication.java
+++ src/main/java/de/kanoune/photoappapiusers/PhotoAppApiUsersApplication.java
@@ -4,10 +4,13 @@ import de.kanoune.photoappapiusers.shared.FeignErrorDecoder;
 import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
@@ -19,6 +22,8 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+//@EnableHystrix
+//@EnableCircuitBreaker
 public class PhotoAppApiUsersApplication {
 
 	public static void main(String[] args) {
--- src/main/java/de/kanoune/photoappapiusers/model/feignClient/AlbumsServiceClient.java
+++ src/main/java/de/kanoune/photoappapiusers/model/feignClient/AlbumsServiceClient.java
@@ -2,17 +2,54 @@ package de.kanoune.photoappapiusers.model.feignClient;
 
 import de.kanoune.photoappapiusers.model.rest.response.AlbumResponse;
 
+import feign.FeignException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.openfeign.FallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@FeignClient(name="albums-ws")
+
+@FeignClient(name="albums-ws", fallbackFactory = AlbumsFallbackFactory.class)
 public interface AlbumsServiceClient {
 
     @GetMapping("/users/{id}/albumss")
     List<AlbumResponse> getAlbums(@PathVariable String id);
-
-
 }
+
+@Component
+class AlbumsFallbackFactory implements FallbackFactory<AlbumsServiceClient> {
+
+
+    @Override
+    public AlbumsServiceClient create(Throwable cause) {
+        return new AlbumsServiceClientFallback(cause);
+    }
+}
+
+@Slf4j
+class AlbumsServiceClientFallback implements AlbumsServiceClient {
+
+    private final Throwable cause;
+
+    AlbumsServiceClientFallback(Throwable throwable) {
+        this.cause = throwable;
+    }
+
+    @Override
+    public List<AlbumResponse> getAlbums(String id) {
+
+        if(cause instanceof FeignException && ((FeignException) cause).status() == 404) {
+            log.error("404 error took place when getAlbums was called with userId: "
+            + id + ". Error message: " + cause.getLocalizedMessage());
+        } else {
+            log.error("Other error took place: " + cause.getLocalizedMessage());
+        }
+        return new ArrayList<>();
+    }
+}
+
